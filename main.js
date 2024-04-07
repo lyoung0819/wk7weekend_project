5e4753c18255cd530ffd30310bc3430102be5027
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-07 15:43:39 -0500

    data functions added. couple bugs with data display on currency and languages

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ findCountryForm.addEventListener('submit', findCountry) // add event listener fo
 function findCountry(e){
     e.preventDefault(); // preventing the form default (refreshing with form data as query params)
     // console.log(e);
-    
+
     // Get the values from the input 
     let countryName = document.getElementById('countryInput').value; // grab value from input element
     console.log(`Looking for information on the country ${countryName}...`)
@@ -20,9 +20,40 @@ function findCountry(e){
     //Make the HTTP get request to the above url and log the data 
     fetch(url) // fetch returns a promise, so we need to tell it what to do once the promise is fulfilled
         .then( res => res.json()) // once the url is fetched,, then we want to log the response obj in json format (this will also retur a promise so we need another .then) 
-        .then( data => console.log(data) ) // need a func to keep the data stored, so we are taking in the data and storing it - data = return console.log(data)
+        .then( data => displayCountry(data) ) // need a func to keep the data stored, so we are taking in the data and storing it - data = return console.log(data)
         .catch(err => console.error(err)) // if something breaks, log an error
 
-
-
 }
+
+// Callback function for findCountry that will accept country data and insert into the display on the webpage
+function displayCountry(data){
+    // grab each area from the HTML
+    let cname = document.getElementById('cname')
+    let currency = document.getElementById('curr')
+    let language = document.getElementById('lang')
+    let capital = document.getElementById('capcity')
+    //clear data
+    cname.innerHTML = '';
+    currency.innerHTML = '';
+    language.innerHTML = '';
+    capital.innerHTML = '';
+
+    // if country can't be found:
+    if (!data.length){
+        cname.innerHTML = 'Country Not Found'
+        return
+    }
+    
+
+    for (let countryInfo of data){
+        newDataCell(cname, countryInfo.name.common)
+        newDataCell(currency, `${countryInfo.name.common}'s currency is ${countryInfo.currencies}`)
+        newDataCell(language, `The people who live in ${countryInfo.name.common} speak ${countryInfo.languages}`)
+        newDataCell(capital, `The capital city of ${countryInfo.name.common} is ${countryInfo.capital}`)
+    }
+}
+
+// helper function 
+function newDataCell(element, value){
+    element.innerHTML = value ?? '-'
+ }
