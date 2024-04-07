019c5b2bc525fb4ec9f9e4b4cded89de17b0a07e
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-07 15:08:04 -0500

    log country data in console

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// console.log('Hello from main.js')
 
 // Get the find countries form and add submit event listener
 let findCountryForm = document.getElementById('find-country-form'); // grab the specific form we need
@@ -7,11 +6,23 @@ findCountryForm.addEventListener('submit', findCountry) // add event listener fo
 // Event Listener to get country data and display on the page
 function findCountry(e){
     e.preventDefault(); // preventing the form default (refreshing with form data as query params)
-    console.log(e);
+    // console.log(e);
+    
     // Get the values from the input 
     let countryName = document.getElementById('countryInput').value; // grab value from input element
     console.log(`Looking for information on the country ${countryName}...`)
-// Building URL for the API req
+
+
+    // Building URL for the API req
     const url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
     console.log(url)
+
+    //Make the HTTP get request to the above url and log the data 
+    fetch(url) // fetch returns a promise, so we need to tell it what to do once the promise is fulfilled
+        .then( res => res.json()) // once the url is fetched,, then we want to log the response obj in json format (this will also retur a promise so we need another .then) 
+        .then( data => console.log(data) ) // need a func to keep the data stored, so we are taking in the data and storing it - data = return console.log(data)
+        .catch(err => console.error(err)) // if something breaks, log an error
+
+
+
 }
