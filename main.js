09de1128b2ac20d9d8500b0e68205a7b7137eb30
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-04-07 15:02:45 -0500

    event listener and API setup

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hello from main.js')
+// console.log('Hello from main.js')
+
+// Get the find countries form and add submit event listener
+let findCountryForm = document.getElementById('find-country-form'); // grab the specific form we need
+findCountryForm.addEventListener('submit', findCountry) // add event listener for a user to click 
+
+// Event Listener to get country data and display on the page
+function findCountry(e){
+    e.preventDefault(); // preventing the form default (refreshing with form data as query params)
+    console.log(e);
+    // Get the values from the input 
+    let countryName = document.getElementById('countryInput').value; // grab value from input element
+    console.log(`Looking for information on the country ${countryName}...`)
+// Building URL for the API req
+    const url = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`
+    console.log(url)
+}
